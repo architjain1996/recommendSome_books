3450dc93b729e8153c0d7e2ca4c02cf8519aa441
architjain1996 <90087674+architjain1996@users.noreply.github.com>
2021-09-30 04:13:37 +0530

    updated app.js with some more data

--- src/App.js
+++ src/App.js
@@ -4,54 +4,87 @@ import { useState } from "react";
 
 const bookDB = {
   javascript: [
-    { name: "Javascript made easy", rating: "4/5" },
-    { name: "Javascript everything you need to know", rating: "3.5/5" }
+    { name: "Javascript made easy", rating: "4/5", author: "Tanay Pratap" },
+    {
+      name: "Javascript everything you need to know",
+      rating: "3.5/5",
+      author: "Akshay Saini"
+    },
+    { name: "Basic of javaScript", rating: "4/5", author: "Akshay Saini" }
   ],
 
   fiction: [
     {
       name: "Adventures of Meluha",
-      rating: "5/5"
+      rating: "5/5",
+      author: "Nitin"
     },
     {
       name: "Harry Potter and the Sorcerer's Stone",
-      rating: "4.5/5"
+      rating: "4.5/5",
+      author: "J.K. Rowling"
+    },
+    {
+      name: "Game of Thrones",
+      rating: "4.5/5",
+      author: "George R.R. Martin"
     }
   ],
   business: [
     {
       name: "Wall Street",
-      rating: "3.5/5"
+      rating: "3.5/5",
+      author: "Robert"
     },
     {
       name: "MoneyControl",
-      rating: "5/5"
+      rating: "5/5",
+      author: "Money Control"
+    },
+    {
+      name: "Outlook",
+      rating: "5/5",
+      author: "Microsoft"
     }
   ],
   Detective: [
-    { name: "Hardy Boys", rating: "4/5" },
-    { name: "Nancy Drew Files", rating: "5/5" },
-    { name: "Sherlock Homles", rating: "4.5/5" }
+    { name: "Hardy Boys", rating: "4/5", author: "Jim" },
+    { name: "Nancy Drew Files", rating: "5/5", author: "Nancy" },
+    { name: "Sherlock Homles", rating: "4.5/5", author: "Sherlock" }
   ],
 
   Fantasy: [
     {
       name: "Lord of the Rings",
-      rating: "4.5/5"
+      rating: "4.5/5",
+      author: "Benedict"
     },
     {
       name: "Game of Thrones",
-      rating: "4.5/5"
+      rating: "4.5/5",
+      author: "George R.R. Martin"
+    },
+    {
+      name: "Adventures of Tin Tin",
+      rating: "4.5/5",
+      author: "Tin Tin"
     }
   ],
   Computer: [
     {
       name: "C/C++",
-      rating: "4.5/5"
+      rating: "4.5/5",
+      author: "Bjarne Strustroup"
     },
     {
       name: "Java made Easy",
-      rating: "4.5/5"
+      rating: "4.5/5",
+      author: "Ryan Gosling"
+    },
+    {
+      name: "Computer Architecture",
+      rating: "4.5/5",
+      author: "Sandeep Upadhyay"
     }
   ]
 };
